68c80f69524de583cf7f82e15bbc57cef6db3069
hao.yan <hao.yan@dajie-inc.com>
2015-12-09 19:23:38 +0800

    添加激活账户接口

--- src/main/java/cn/sse/bupt/service/UserService.java
+++ src/main/java/cn/sse/bupt/service/UserService.java
@@ -6,11 +6,13 @@ import cn.sse.bupt.model.UserModel;
  * Created by hao.yan on 2015/12/1.
  */
 public interface UserService {
-    public int register(UserModel userModel);
+    public long register(UserModel userModel);
 
     public boolean login(String username, String password);
 
     public boolean hasRegistered(String username);
 
     public int logout(UserModel userModel);
+
+    public int activeAccount(long uid);
 }
