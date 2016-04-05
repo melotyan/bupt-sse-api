0e5913ecc7142576270bf3604f1d906c48be0d2d
melot <melotyan@users.noreply.github.com>
2016-04-05 08:59:59 +0800

    添加招标和竞标两个service接口声明

---
+++ src/main/java/cn/sse/bupt/service/InutatccmOfTenderService.java
@@ -0,0 +1,18 @@
+package cn.sse.bupt.service;
+
+import cn.sse.bupt.model.InutatccmOfTenderModel;
+
+import java.util.List;
+
+/**
+ * Created by melot on 2016/4/4.
+ */
+public interface InutatccmOfTenderService {
+    public int createTenderInfo(InutatccmOfTenderModel inutatccmOfTenderModel);
+
+    public int editTenderInfo(int id, String title, String content, String fileUrl);
+
+    public int deleteTenderInfo(int id);
+
+    public List<InutatccmOfTenderModel> listTenderInfos(int page, int pageSize);
+}
---
+++ src/main/java/cn/sse/bupt/service/TenderService.java
@@ -0,0 +1,14 @@
+package cn.sse.bupt.service;
+
+import cn.sse.bupt.model.TenderModel;
+
+import java.util.List;
+
+/**
+ * Created by melot on 2016/4/4.
+ */
+public interface TenderService {
+    public int competitiveTender(int uid, int tid, String title, String fileUrl);
+
+    public List<TenderModel> listTenderByTid(int tid);
+}
